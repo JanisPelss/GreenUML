155b378d65c8095076e6e39cca7c1969ec9eb8a9
Ernests1992 <ernests@neolain.lv>
2016-08-23 12:21:08 +0300

    Update UmlLog.java

--- Green/src/edu/buffalo/cse/green/UmlLog.java
+++ Green/src/edu/buffalo/cse/green/UmlLog.java
@@ -1,4 +1,4 @@
-package edu.buffalo.cse.green;
+ppackage edu.buffalo.cse.green;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
@@ -7,49 +7,35 @@ import java.util.logging.SimpleFormatter;
 
 public class UmlLog {
 	   
-
- public static int i=0;
-
+	public static int i = 0;
 	
-	public static void kek(String lmao){
-	
-		
+	public static void addToLog(String message) {
 	    Logger logger = Logger.getLogger("MyLog");  
 	    FileHandler fh;  
 	    logger.setUseParentHandlers(false);
-	    if(i==0){
-	    try {  
+	    if (i == 0) {
+	    	try {  
+	    		// This block configures the logger with handler and formatter  
+	    		fh = new FileHandler(System.getProperty("user.dir") + "GreenUmlLog.log");  
+	    		logger.addHandler(fh);
+	    		SimpleFormatter formatter = new SimpleFormatter();  
+	    		fh.setFormatter(formatter);  
 
-	        // This block configure the logger with handler and formatter  
-	        fh = new FileHandler(System.getProperty("user.dir")+"GreenUmlLog.log");  
-	        logger.addHandler(fh);
-	        SimpleFormatter formatter = new SimpleFormatter();  
-	        fh.setFormatter(formatter);  
-
-	        // the following statement is used to log any messages  
-	       logger.info("GreenUML debug log");  
-
-	    } catch (SecurityException e) {  
-	        e.printStackTrace();  
-	    } catch (IOException e) {  
-	        e.printStackTrace();  
-	    }  
-	    i++;
+	    		// The following statement is used to log any messages  
+	    		logger.info("GreenUML debug log");  
+	    	} 
+	    	
+	    	catch (SecurityException e) {  
+	    		e.printStackTrace();  
+	    	} 
+	    	
+	    	catch (IOException e) {  
+	    		e.printStackTrace();  
+	    	}  
+	    	
+	    	i++;
+	    }
+	    
+	    logger.info(message);	
 	}
-	    logger.info(lmao);
-		
-		
-		
-	}
-
- public void kek2 (String la){
-	 
- }
-
-
-
-
-
-	
-
 }
